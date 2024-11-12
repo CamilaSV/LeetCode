e4037ac1d496eb6c39bc6a9176be639fd04ff93b
Camila Suarez Viltres <44325177+CamilaSV@users.noreply.github.com>
2024-11-12 11:22:39 -0700

    Time: 1 ms (63.89%) | Memory: 52.8 MB (33.39%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -4,17 +4,14 @@
  */
 
 var removeDuplicates = function(nums) {
-    let k = 0;
+    let k = 1;
 
     for ( let i = 0; i < nums.length; i++){
-        if (nums[i] === nums[i-1]){
-            nums.splice(i, 1);
-            i--;
-        }
-        else{
+        if (nums[i] !== nums[k-1]){
+            nums[k] = nums[i];
             k++;
         }
     }
 
-    return k;
+    return k--;
 };
